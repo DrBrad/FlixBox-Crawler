909d914ba5d6cb83000dab39ff9b11d0ab350d10
Brad <brad@bradeagle.com>
2023-07-24 07:06:33 -0600

    Update Hash.java

--- src/main/java/unet/fcrawler/commands/Hash.java
+++ src/main/java/unet/fcrawler/commands/Hash.java
@@ -10,7 +10,9 @@ public class Hash {
     public Hash(List<String> cmd)throws Exception {
         File f = new File(cmd.get(0));
         if(f.isDirectory()){
-            for(File n : f.listFiles()){
+            File[] files = f.listFiles();
+            Arrays.sort(files, (a, b) -> a.getName().compareTo(b.getName()));
+            for(File n : files){
                 System.out.println(n.getName()+" > "+new CheckSum(n).execute());
             }
         }else{
